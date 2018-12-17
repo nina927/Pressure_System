381735b3865f78178c4b259e81bed8268eec21f3
Nina <selenakhoury6@gmail.com>
2018-12-16 19:20:02 -0500

    Added comments for clarification and a macro for setting timer interrupt flag

--- Pressure_System.c
+++ Pressure_System.c
@@ -4,8 +4,8 @@
 #include <avr/power.h>
 #include "pinDefines.h"
 
-#define M0S          0
-#define M0D          1
+#define M0S          0       // Motor 0 Step
+#define M0D          1       // Motor 0 Direction
 #define M0S_DDR      DDRD
 #define M0D_DDR      DDRD
 #define M0S_PORT     PORTD
@@ -16,22 +16,24 @@
 #define Button_PORT  PORTD
 #define Button_PIN   PIND
 
-#define BV(bit)               (1 << bit)
+#define BV(bit)                (1 << bit)
 #define set_bit(sfr, bit)     (_SFR_BYTE(sfr) |= BV(bit))
 #define clear_bit(sfr, bit)   (_SFR_BYTE(sfr) &= ~BV(bit))
-#define toggle_bit(sfr, bit)  (_SFR_BYTE(sfr) ^= BV(bit))  
+#define toggle_bit(sfr, bit)  (_SFR_BYTE(sfr) ^= BV(bit))
+#define turn_on_interrupt       set_bit(TIMSK1, OCIE1A);
+#define turn_off_interrupt      clear_bit(TIMSK1, OCIE1A);
 
 #define MICROSTEPS    16
-#define PRESSURE_INC  71      //set for 0.25 Pa and 3mL syringe
-#define NUMBER_INC    8       // set for +/-2 Pa
-#define OVERSLOP      200      
-#define DELAY         2499     // frequency=100Hz maybe??
+#define PRESSURE_INC  71      /* steps to apply one pressure increment (set for 0.25 Pa with  3mL syringe) */
+#define NUMBER_INC    8       /* number of pressure increments to get to max pressure (set for +/-2 Pa) */
+#define OVERSLOP      200     /* steps to overcome hysteresis of syringe piston */     
+#define DELAY         2499     /* frequency=100Hz maybe?? */
 
 
 // -------- Global Variables --------- //
 
 volatile uint16_t stepCounter = 0;
-volatile uint8_t  ButtonState = 0;
+volatile uint8_t  ButtonState = 0; // WHAT IS THIS
 
 // ------ Functions --------- //
 
@@ -49,13 +51,10 @@ void initTimer(void) {
   set_bit(TCCR1B,CS10);              // no prescaler
   OCR1A = DELAY;
   sei();                            // enable global interrupts
-  // Notice we haven't set the timer1 interrupt flag yet
+  /*  Notice we haven't set the timer1 interrupt flag yet */
 }
 
-/* It would be a good idea to describe what this ISR does, just to be clear.
- * It appears you are trying to increment a counter and possibly toggle an LED?
- * The toggle may be too fast to be noticable, given it would take a cycle or two 
- * to actually toggle. */
+/* ISR toggles motor step pin  */
 ISR(TIMER1_COMPA_vect) {
   toggle_bit(M0S_PORT,M0S);
   stepCounter++;
@@ -64,7 +63,7 @@ ISR(TIMER1_COMPA_vect) {
 void waitForButton(void){ 
   while(bit_is_set(Button_PIN, Button)){  /* Depending upon the return value, this may be skipped and not wait for it to be set */
   }
-  _delay_ms(20);
+  _delay_ms(20); 
   while(bit_is_clear(Button_PIN, Button)){   
   }
 }
@@ -77,10 +76,10 @@ void takeSteps(uint16_t howManySteps, uint8_t direction) {
     set_bit(M0D_PORT,M0D);
   }
   stepCounter = 0;               // initialize steps
-  set_bit(TIMSK1, OCIE1A); 	// turn on interrupts, stepping    /* You may want to make a macro for turning this interrupt on for ease of reading */
+  turn_on_interrupt;    	// start stepping
   while (!(stepCounter == howManySteps*2*MICROSTEPS)) {;
   }                            // wait
-  clear_bit(TIMSK1,OCIE1A);   // turn back off
+  turn_off_interrupt;          // stop stepping
 }
 
 
@@ -88,32 +87,37 @@ void pressureCycle(void){
 
   uint8_t forward = 0;
   uint8_t backward = 1;
-	
+
+  /* Move back and forth to avoid slop */
   takeSteps(OVERSLOP,backward);
+  _delay_ms(500); // avoid skipping steps on direction change 
   takeSteps(OVERSLOP,forward);
   waitForButton();
 	
-  // Move forward to 10 Pa, 2 Pa
+  /*  Move forward from zero to max pressure */
   uint8_t INC = 1;
-  while (INC <= NUMBER_INC){		/* Be careful about overflow here since you are using unsigned numbers */
+  while (INC <= NUMBER_INC){		/* Be careful about overflow  */
     takeSteps(PRESSURE_INC, forward);
     waitForButton();
     INC++; 
   }	 
-	
+
+  /* Move backwards from max to min pressure */	
   INC = 1;
-  while (INC <= NUMBER_INC*2){ 		/* Be careful about overflow here since you are using unsigned numbers */
+  while (INC <= NUMBER_INC*2){		/* Be careful about overflow  */
     takeSteps(PRESSURE_INC,backward);
     INC++;
   }
-	
+  
+  /* Move back and forth to avoid slop */
   takeSteps(OVERSLOP,backward);
-  _delay_ms(500); // Direction change avoid skip steps
+  _delay_ms(500);
   takeSteps(OVERSLOP,forward);
   waitForButton();
-	
+
+  /* Move forward from min pressure to zero */
   INC = 1;
-  while (INC <= NUMBER_INC){		/* Be careful about overflow here since you are using unsigned numbers */
+  while (INC <= NUMBER_INC){		/* Be careful about overflow  */
     takeSteps(PRESSURE_INC,forward);
     waitForButton();
     INC++;
