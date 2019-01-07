2f400340288e6b7be86a609599f4db2b07faff71
Nina <selenakhoury6@gmail.com>
2019-01-06 22:04:31 -0500

    Added helpful comments, separated pressureCycle into more functions for readability

--- Pressure_System.c
+++ Pressure_System.c
@@ -2,16 +2,15 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <avr/power.h>
-#include "pinDefines.h"
 
-#define M0S          0       // Motor 0 Step
-#define M0D          1       // Motor 0 Direction
-#define M0S_DDR      DDRD
+#define M0S          0       /*  Motor Step Pin */
+#define M0D          1       /*  Motor Direction Pin */
+#define M0S_DDR      DDRD    
 #define M0D_DDR      DDRD
 #define M0S_PORT     PORTD
 #define M0D_PORT     PORTD
 
-#define Button       2
+#define Button       2       /*  Button Pin, other side goes to ground */
 #define Button_DDR   DDRD  
 #define Button_PORT  PORTD
 #define Button_PIN   PIND
@@ -20,37 +19,39 @@
 #define set_bit(sfr, bit)     (_SFR_BYTE(sfr) |= BV(bit))
 #define clear_bit(sfr, bit)   (_SFR_BYTE(sfr) &= ~BV(bit))
 #define toggle_bit(sfr, bit)  (_SFR_BYTE(sfr) ^= BV(bit))
-#define turn_on_interrupt       set_bit(TIMSK1, OCIE1A);
-#define turn_off_interrupt      clear_bit(TIMSK1, OCIE1A);
+#define turn_on_interrupt       set_bit(TIMSK1, OCIE1A);    /*  Set timer 1 interrupt flag */
+#define turn_off_interrupt      clear_bit(TIMSK1, OCIE1A);  /*  Clear timer 1 interrupt flag */
 
-#define MICROSTEPS    16
+#define MICROSTEPS    16      /* 1/16th microstepping */
 #define PRESSURE_INC  71      /* steps to apply one pressure increment (set for 0.25 Pa with  3mL syringe) */
 #define NUMBER_INC    8       /* number of pressure increments to get to max pressure (set for +/-2 Pa) */
 #define OVERSLOP      200     /* steps to overcome hysteresis of syringe piston */     
 #define DELAY         2499     /* frequency=100Hz maybe?? */
 
+#define FORWARD       0      /* Forward direction bit */
+#define BACKWARD      1      /* Backward direction bit */
 
-// -------- Global Variables --------- //
+/*  -------- Global Variables --------- */
 
 volatile uint16_t stepCounter = 0;
-volatile uint8_t  ButtonState = 0; // WHAT IS THIS
+volatile uint8_t  buttonState = 0;
 
-// ------ Functions --------- //
+/* ------ Functions --------- */
 
 void initMotorsButton(void){
-  set_bit(M0S_DDR,M0S);
-  set_bit(M0D_DDR,M0D);
-  clear_bit(Button_DDR,Button);
-  set_bit(Button_PORT, Button);
+  set_bit(M0S_DDR,M0S);     /* Step pin in output mode */
+  set_bit(M0D_DDR,M0D);     /* Direction pin in output mode */
+  clear_bit(Button_DDR,Button); /* Set button pin in input mode */
+  set_bit(Button_PORT, Button); /* Set pullup resistor  */
   _delay_ms(1000);
 }
 
 void initTimer(void) {
-  clock_prescale_set(clock_div_1);   // CPU clock 8 MHz
-  set_bit(TCCR1B,WGM12);             // CTC mode
-  set_bit(TCCR1B,CS10);              // no prescaler
+  clock_prescale_set(clock_div_1);   /*  CPU clock 8 MHz */
+  set_bit(TCCR1B,WGM12);             /*  CTC mode */
+  set_bit(TCCR1B,CS10);              /*  no prescaler */
   OCR1A = DELAY;
-  sei();                            // enable global interrupts
+  sei();                            /* enable global interrupts */
   /*  Notice we haven't set the timer1 interrupt flag yet */
 }
 
@@ -60,86 +61,67 @@ ISR(TIMER1_COMPA_vect) {
   stepCounter++;
 }
 
-void waitForButton(void){ 
-  while(bit_is_set(Button_PIN, Button)){  /* Depending upon the return value, this may be skipped and not wait for it to be set */
-  }
-  _delay_ms(20); 
-  while(bit_is_clear(Button_PIN, Button)){   
-  }
+
+void waitForButton(void){
+  while(bit_is_set(Button_PIN, Button)){    /* while button is not pressed */
+  }                                         /* wait for button to be pressed */
+  _delay_ms(20);                            /* Debounce delay */
+  while(bit_is_clear(Button_PIN, Button)){  /* while button is pressed */
+  }                                         /*  wait until button is let go  */
 }
 
 void takeSteps(uint16_t howManySteps, uint8_t direction) {
   if (!direction) {
-    clear_bit(M0D_PORT,M0D);
+    clear_bit(M0D_PORT,M0D);  /* set in forward direction */
   }
   else {
-    set_bit(M0D_PORT,M0D);
+    set_bit(M0D_PORT,M0D);    /* set in backward direction */
   }
-  stepCounter = 0;               // initialize steps
-  turn_on_interrupt;    	// start stepping
+  stepCounter = 0;            /*  initialize step counter */
+  turn_on_interrupt;          /*  start stepping */
   while (!(stepCounter == howManySteps*2*MICROSTEPS)) {;
-  }                            // wait
-  turn_off_interrupt;          // stop stepping
+  }                            /* wait */
+  turn_off_interrupt;          /* stop stepping */
 }
 
+void overslopAvoidance(void){
+  /* Move back and forth to avoid slop */
+  takeSteps(OVERSLOP,BACKWARD);
+  _delay_ms(500);    /* avoid skipping steps on direction change */
+  takeSteps(OVERSLOP,FORWARD);
+  waitForButton();
+}
+
+void moveForward(void){
+  uint8_t inc = 1;
+  while (inc <= NUMBER_INC){
+    takeSteps(PRESSURE_INC, FORWARD); /* Be careful about overflow */
+    waitForButton();
+    inc++;
+  }
+}
+
+void moveBackward(void){
+  uint8_t inc = 1;
+  while (inc <= 2*NUMBER_INC){
+    takeSteps(PRESSURE_INC, BACKWARD); /* Be careful about overflow */
+    inc++;
+  }
+}
 
 void pressureCycle(void){
-
-  uint8_t forward = 0;
-  uint8_t backward = 1;
-
-  /* Move back and forth to avoid slop */
-  takeSteps(OVERSLOP,backward);
-  _delay_ms(500); // avoid skipping steps on direction change 
-  takeSteps(OVERSLOP,forward);
-  waitForButton();
-	
-  /*  Move forward from zero to max pressure */
-  uint8_t INC = 1;
-  while (INC <= NUMBER_INC){		/* Be careful about overflow  */
-    takeSteps(PRESSURE_INC, forward);
-    waitForButton();
-    INC++; 
-  }	 
-
-  /* Move backwards from max to min pressure */	
-  INC = 1;
-  while (INC <= NUMBER_INC*2){		/* Be careful about overflow  */
-    takeSteps(PRESSURE_INC,backward);
-    INC++;
-  }
-  
-  /* Move back and forth to avoid slop */
-  takeSteps(OVERSLOP,backward);
-  _delay_ms(500);
-  takeSteps(OVERSLOP,forward);
-  waitForButton();
-
-  /* Move forward from min pressure to zero */
-  INC = 1;
-  while (INC <= NUMBER_INC){		/* Be careful about overflow  */
-    takeSteps(PRESSURE_INC,forward);
-    waitForButton();
-    INC++;
-  }
-	
+  overslopAvoidance();   /* Move back and forth to avoid slop */
+  moveForward();        /* Move forward from zero to max pressure */
+  moveBackward();       /* Move backward from max to min pressure */
+  overslopAvoidance();  /* Move back and forth to avoid slop */
+  moveForward();       /* Move forward from min pressure to zero */	
 }
 
 
-int main(void) {
+int main(void){
   initMotorsButton();
   initTimer();
-  pressureCycle();
-  return 0;		
-  /* With embedded systems, you usually don't want a reutrn here
-  With GCC, you can use the following to make a non returnable function
-  and cause it to not remove an infinite loop at the end of your function (Most likley)
-
-  __attribute__((noreturn)) void main (void ){ 
-  
-  	while( 1 );	Loop to catch errors; get stuck here. maybe watchdog will reset otherwise if you enabeld it
-  }
-
-  */
+  pressureCycle();		
+  return 0;
 }
 
